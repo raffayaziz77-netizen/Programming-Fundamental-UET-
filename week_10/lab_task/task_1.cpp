d69d99c5fb6f018fadcbf067a0985412136ab2c9
raffay aziz <raffayaziz77@gmail.com>
2026-04-26 15:34:28 +0500

    task_1 added

---
+++ week_10/lab_task/task_1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int sum(int n1, int n2)
+{
+    return n1+n2;
+}
+int main()
+{
+    cout<<"Enter numbers to sum: ";
+    int num1,num2;
+    cin>>num1>>num2;
+    cout<<"sum equals: "<<sum(num1,num2);
+}
